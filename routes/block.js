93c1eec54b155876ba30a7e0ca98686213206bcf
gobitfly <you@example.com>
2017-03-09 15:46:56 +0100

    Added uncle support

--- routes/block.js
+++ routes/block.js
@@ -49,4 +49,35 @@ router.get('/:block', function(req, res, next) {
   
 });
 
+router.get('/uncle/:hash/:number', function(req, res, next) {
+  
+  var config = req.app.get('config');  
+  var web3 = new Web3();
+
+  web3.setProvider(new web3.providers.IpcProvider(config.backend, require('net')));
+  
+  async.waterfall([
+    function(callback) {
+      web3.eth.getUncle(req.params.hash, req.params.number, true, function(err, result) {
+        callback(err, result);
+      });
+    }, function(result, callback) {
+      if (!result) {
+        return next({name : "UncleNotFoundError", message : "Uncle not found!"});
+      }
+
+      callback(null, result);
+    }
+  ], function(err, uncle) {
+    if (err) {
+      return next(err);
+    }
+     
+    console.log(uncle);
+    
+    res.render('uncle', { uncle: uncle, blockHash: req.params.hash });
+  });
+  
+});
+
 module.exports = router;
