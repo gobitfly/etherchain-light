8ebc7725cf5ec0169d67183264c781cb300c06b7
Christopher Franko <chrisjfranko@gmail.com>
2017-03-22 14:59:23 -0400

    fix no accounts error

--- routes/accounts.js
+++ routes/accounts.js
@@ -18,7 +18,7 @@ router.get('/:offset?', function(req, res, next) {
       
       var data = {};
       
-      if (accounts.length === 0) {
+      if (accounts === null) {
         return callback({name:"NoAccountsFound", message: "Chain contains no accounts."});
       }
       
