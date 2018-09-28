e9a7aa65e248061d23e8ed49b923d7f44d9b63e7
cdetrio <cdetrio@gmail.com>
2018-09-28 16:11:47 -0400

    pending txs from geth have null receipt

--- routes/tx.js
+++ routes/tx.js
@@ -8,12 +8,12 @@ var abi = require('ethereumjs-abi');
 var abiDecoder = require('abi-decoder');
 
 router.get('/pending', function(req, res, next) {
-  
-  var config = req.app.get('config');  
+
+  var config = req.app.get('config');
   var web3 = new Web3();
   web3extended(web3);
   web3.setProvider(config.provider);
-  
+
   async.waterfall([
     function(callback) {
       var txs = [];
@@ -29,13 +29,13 @@ router.get('/pending', function(req, res, next) {
     if (err) {
       return next(err);
     }
-    
+
     res.render('tx_pending', { txs: txs });
   });
 });
 
 
-router.get('/submit', function(req, res, next) {  
+router.get('/submit', function(req, res, next) {
   res.render('tx_submit', { });
 });
 
@@ -43,11 +43,11 @@ router.post('/submit', function(req, res, next) {
   if (!req.body.txHex) {
     return res.render('tx_submit', { message: "No transaction data specified"});
   }
-  
-  var config = req.app.get('config');  
+
+  var config = req.app.get('config');
   var web3 = new Web3();
   web3.setProvider(config.provider);
-  
+
   async.waterfall([
     function(callback) {
       web3.eth.sendRawTransaction(req.body.txHex, function(err, result) {
@@ -64,28 +64,28 @@ router.post('/submit', function(req, res, next) {
 });
 
 router.get('/:tx', function(req, res, next) {
-  
-  var config = req.app.get('config');  
+
+  var config = req.app.get('config');
   var web3 = new Web3();
   web3.setProvider(config.provider);
-  
+
   var db = req.app.get('db');
-  
+
   async.waterfall([
     function(callback) {
       web3.eth.getTransaction(req.params.tx, function(err, result) {
         callback(err, result);
       });
     }, function(result, callback) {
-      
+
       if (!result || !result.hash) {
         return callback({ message: "Transaction hash not found" }, null);
       }
-      
+
       web3.eth.getTransactionReceipt(result.hash, function(err, receipt) {
         callback(err, result, receipt);
       });
-    }, function(tx, receipt, callback) {  
+    }, function(tx, receipt, callback) {
       db.get(tx.to, function(err, value) {
         callback(null, tx, receipt, null, value);
       });
@@ -94,7 +94,7 @@ router.get('/:tx', function(req, res, next) {
     if (err) {
       return next(err);
     }
-     
+
     // Try to match the tx to a solidity function call if the contract source is available
     if (source) {
       tx.source = JSON.parse(source);
@@ -109,10 +109,14 @@ router.get('/:tx', function(req, res, next) {
     }
     tx.traces = [];
     tx.failed = false;
-    tx.gasUsed = receipt.gasUsed;
-    if (!tx.to) {
-      tx.contractAddress = receipt.contractAddress;
+    // pending transactions from geth have null receipts
+    if (receipt) {
+      tx.gasUsed = receipt.gasUsed;
+      if (!tx.to) {
+        tx.contractAddress = receipt.contractAddress;
+      }
     }
+
     if (traces != null) {
     traces.forEach(function(trace) {
         tx.traces.push(trace);
@@ -125,18 +129,18 @@ router.get('/:tx', function(req, res, next) {
         }
       });
     }
-    // console.log(tx.traces);    
+    // console.log(tx.traces);
     res.render('tx', { tx: tx });
   });
-  
+
 });
 
 router.get('/raw/:tx', function(req, res, next) {
-  
-  var config = req.app.get('config');  
+
+  var config = req.app.get('config');
   var web3 = new Web3();
   web3.setProvider(config.provider);
-  
+
   async.waterfall([
     function(callback) {
       web3.eth.getTransaction(req.params.tx, function(err, result) {
@@ -151,7 +155,7 @@ router.get('/raw/:tx', function(req, res, next) {
     if (err) {
       return next(err);
     }
-    
+
     tx.traces = traces;
 
     res.render('tx_raw', { tx: tx });
