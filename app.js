243ee6815d8463466be0c4f18158c7aa47c03b91
Sergii Bomko <aquila.ua@gmail.com>
2018-10-06 14:21:44 +0200

    updates levelup and leveldown for node10 compatibility

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var search = require('./routes/search');
 var config = new(require('./config.js'))();
 
 var levelup = require('levelup');
-var db = levelup('./data');
+var leveldown = require('leveldown');
+var db = levelup(leveldown('./data'));
 
 var app = express();
 
