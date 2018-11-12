c3ae7acbad02c1a9fd4ee109d7e1cfac9f8a0e97
cdetrio <cdetrio@gmail.com>
2018-11-12 08:30:33 -0500

    update account names

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ var config = function (endpoint) {
   this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css";
 
   this.names = {
-    "0xefadf166849f06eee4b44ddb78e4162580f436b1": "ewasm Faucet",
-    "0x031159df845ade415202e6da299223cb640b9db0": "ewasm miner 1",
+    "0x9fa4f23079bae7a7a5c392b67e18093310315bfc": "ewasm Faucet",
+    "0xa8C3eEb2915373139bcfc287D4ae9E660d734881": "ewasm miner",
     "0x000000000000000000000000000000000000000a": "sentinel contract",
     "0x000000000000000000000000000000000000000b": "evm2wasm contract"
   }
