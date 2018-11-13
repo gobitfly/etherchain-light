a17c26d28679d205829fa70586d1e8348ccfc09f
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2018-11-13 15:55:47 -0600

    fix the config.py to inject private key, no longer configuring endpoint in config.js

--- config.py
+++ config.py
@@ -2,19 +2,23 @@ import argparse, json, sys, binascii
 
 parser = argparse.ArgumentParser(description='Preload a Geth genesis with contract accounts')
 
-parser.add_argument('--rpc-endpoint', type=str, nargs='?',
-                            help='URL for rpc endpoint')
 parser.add_argument('--template', type=str, nargs='?',
                             help='path to the genesis source template')
+parser.add_argument('--faucet-address', type=str, nargs='?',
+                            help='faucet address')
+parser.add_argument('--private-key', type=str, nargs='?',
+                            help='faucet private key')
 
 parse_args = parser.parse_args()
 
 template = parse_args.template
-endpoint = parse_args.rpc_endpoint
+faucet_address = parse_args.faucet_address
+private_key = parse_args.private_key
 
 try:
   with open(template) as f:
-    template = f.read().replace('{rpcEndpoint}', endpoint)
+    template = f.read().replace('{privateKey}', private_key)
+    template = template.replace('{faucetAddress}', faucet_address)
     print(template)
 except Exception as e:
   print(e)
