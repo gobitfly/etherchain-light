7feb226944321f563b834ee5879a560913a9c466
Alex Beregszaszi <alex@rtfs.hu>
2018-11-14 22:43:28 +0000

    Ensure ether balances are converted properly

--- utils/ethformatter.js
+++ utils/ethformatter.js
@@ -1,7 +1,7 @@
 var web3 = require('web3');
 
 function formatAmount(amount) {
-  var amountAsString = web3.utils.fromWei(amount, 'ether');
+  var amountAsString = web3.utils.fromWei(Math.floor(amount).toString(), 'ether');
   return amountAsString + " ETH";
 }
 module.exports = formatAmount;
