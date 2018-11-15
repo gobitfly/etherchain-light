2765a607e9f14d2045417d1a1e46be75015350f7
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2018-11-14 21:09:41 -0600

    fix search to handle block numbers correctly

--- routes/search.js
+++ routes/search.js
@@ -7,16 +7,14 @@ router.post('/', function(req, res, next) {
   if (searchString.length > 22 && searchString.substr(0,2) != '0x')
     searchString = '0x' + searchString;
     
-	if (searchString.length === 2) {
-		return next({ message: "Error: Invalid search string!" });
-	} else if (searchString.length < 22) {
-		// Most likely a block number, forward to block id handler
-		res.redirect('/explorer/block/' + searchString);
-	} else if (searchString.length == 66) {
-		res.redirect('/explorer/tx/' + searchString);
-	} else if (searchString.length == 42) {
-		res.redirect('/explorer/account/' + searchString);
-	} else {
+  if (searchString.length < 22 && !isNaN(searchString.length)) {
+    // Most likely a block number, forward to block id handler
+    res.redirect('/explorer/block/' + searchString);
+  } else if (searchString.length == 66) {
+    res.redirect('/explorer/tx/' + searchString);
+  } else if (searchString.length == 42) {
+    res.redirect('/explorer/account/' + searchString);
+  } else {
     return next({ message: "Error: Invalid search string!" });
   }
 });
